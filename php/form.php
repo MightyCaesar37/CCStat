4eede5534f20845cff8fa2fd83cbe56a26a7feef
JPAshour <jpashour@tamu.edu>
2019-09-07 18:37:30 -0500

    Changed button type in createEvents and worked on form.php

--- php/form.php
+++ php/form.php
@@ -11,8 +11,26 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
+$name = $organization = $time = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = test_input($_POST["name"]);
+    $organization = test_input($_POST["organization"]);
+    $time = test_input($_POST["time"]);
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+  
+
+
 $sql = "INSERT INTO events (name, time, orginization_id)
 VALUES ('Test', '9999-12-31 23:59:59', 1)";
+
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
